4a9b3b75ee6e3484485fbcfdd66650e41bccb0e7
Vytautas Jakutis <vytautas@jakut.is>
2013-06-08 14:19:00 +0300

    example/rservetest.js: use client.exec instead of client.eval

--- example/rservetest.js
+++ example/rservetest.js
@@ -1,12 +1,12 @@
 require('../lib/rserve').connect('localhost', 6311, function(err, client) {
     var command = 'a<-list(1,c("ab","cd"))';
     console.log('evaluating', command);
-    client.eval(command, function(err, ans) {
+    client.exec(command, function(err, ans) {
         console.log('answer', ans);
 
         var command = 'a';
         console.log('evaluating', command);
-        client.eval(command, function(err, ans) {
+        client.exec(command, function(err, ans) {
             console.log('answer', ans);
             client.end();
         });
